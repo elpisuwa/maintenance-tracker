3c6ea753390c4b1f592486d36da4f85f10dce7d1
uwahope007 <uwahope007@gmail.com>
2018-05-11 05:20:50 +0100

    improved login form UI

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,3 +1,15 @@
+window.onload = function(){ 
+    var requestModal = document.getElementById('myModal');
+var btn2 = document.getElementById("requestModal");
+var span = document.getElementsByClassName("close")[0];
+if (btn2!==""){
+btn2.onclick = function() {
+    requestModal.style.display = "inline-block";
+}								
+span.onclick = function() {
+    requestModal.style.display = "none";
+}}
+
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("signupModal");
 var span = document.getElementsByClassName("close")[0];
@@ -6,14 +18,7 @@ btn.onclick = function() {
 }
 span.onclick = function() {
     modal.style.display = "none";
-}
+};
 
-var requestModal = document.getElementById('myModal');
-var btn = document.getElementById("requestModal");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    requestModal.style.display = "inline-block";
-}								
-span.onclick = function() {
-    requestModal.style.display = "none";
+
 }
