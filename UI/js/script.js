26ca331f7bea6285b561d58f00e20ca244b3b73a
uwahope007 <uwahope007@gmail.com>
2018-05-11 02:08:23 +0100

    added users make and view request page

--- UI/js/script.js
+++ UI/js/script.js
@@ -7,3 +7,13 @@ btn.onclick = function() {
 span.onclick = function() {
     modal.style.display = "none";
 }
+
+var requestModal = document.getElementById('myModal');
+var btn = document.getElementById("requestModal");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    requestModal.style.display = "inline-block";
+}								
+span.onclick = function() {
+    requestModal.style.display = "none";
+}
