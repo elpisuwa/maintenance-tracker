8ce2c6eb2c437b132200ede04776bdb23df3ad49
uwahope007 <uwahope007@gmail.com>
2018-05-18 11:50:10 +0100

    fix route bug

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,11 @@
-import requestController from '../controller/requestController';
+import requestController from '../controllers/requestController';
 
 const routes =(app)=>{
 	app.get('/', (req,res)=>{
 		res.status(200).send('Welcome to Maintenance Tracker');
 	});
 	
-	app.post('api/v1/users/requests',requestController.postRequest);
+	app.post('/api/v1/users/requests',requestController.postRequest);
 }	
 
 
