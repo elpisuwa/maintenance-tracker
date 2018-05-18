67e3f702ad0471074aeca242f6a0d6946ad11524
uwahope007 <uwahope007@gmail.com>
2018-05-18 09:44:31 +0100

    bug fix

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -8,6 +8,8 @@ class RequestController{
 
 
 
+
+
 }
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,13 @@
-import requestController from '../controller/requestController';
+import requestController from '../controllers/requestController';
 
 const routes =(app)=>{
 	app.get('/', (req,res)=>{
 		res.status(200).send('Welcome to Maintenance Tracker');
 	});
 	app.get('api/v1/users/requests',requestController.allRequest);
-	app.get('api/v1/users/requests/:requestId', requestController.getRequest);
-	app.post('api/v1/users/requests',requestController.postRequest);
-}	app.put('api/v1/users/requests/:requestId', requestController.updateRequest);
-
+	// app.get('api/v1/users/requests/:requestId', requestController.getRequest);
+	// app.post('api/v1/users/requests',requestController.postRequest);
+	// app.put('api/v1/users/requests/:requestId', requestController.updateRequest);
+}
 
 export default routes;
