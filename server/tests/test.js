7424c05aeba482e44148da3e780552018667dc3d
uwahope007 <uwahope007@gmail.com>
2018-05-18 12:32:43 +0100

    update test file

--- server/tests/test.js
+++ server/tests/test.js
@@ -1,7 +1,9 @@
+import chaiHttp from 'chai-http';
 import chai from 'chai';
 import app from '../app';
 
 const {expect} = chai;
+chai.use(chaiHttp);
 
 describe('POST endpoint for request', ()=>{
 
