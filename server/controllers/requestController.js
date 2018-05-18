fa7944ad7828641f9c1f19bc85ac3525d12ac99d
uwahope007 <uwahope007@gmail.com>
2018-05-18 11:51:55 +0100

    add request controller

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -3,7 +3,19 @@ import data from '../data/request.json';
 
 
 class RequestController{
+  
+  static postRequest(request,response){
+  	const {userId,requestType,requestDescription} = request.body;
+  	 const id = data[data.length-1].id+1;
+  	 const searchRequest = {id, userId, requestType,requestDescription};
 
+  	 data.push(searchRequest);
+  	 return response.status(201).json({Request: data, message: 'request as been added successfully'});
+ 
+
+
+
+  }
 
 
 
