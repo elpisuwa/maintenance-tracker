f2e180ac2cf078cc3511540041b8739724af3c94
uwahope007 <uwahope007@gmail.com>
2018-05-18 14:20:17 +0100

    update request controller

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -4,6 +4,23 @@ import data from '../data/request.json';
 
 class RequestController{
 
+	static updateRequest(request,response){
+		const {requestType,requestDescription} = request.body;
+
+
+		const searchRequest = data.findIndex((userRequest)=>
+			userRequest.id === parseInt(request.params.requestId, 10)
+		);
+
+		
+			data[searchRequest].requestType = request.body.requestType;
+			data[searchRequest].description = request.body.requestDescription;
+
+			return response.status(200).json({message:'Request has been successfully updated', request: data.searchRequest})
+		
+
+	}
+
 
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,8 @@ const routes =(app)=>{
 	app.get('/', (req,res)=>{
 		res.status(200).send('Welcome to Maintenance Tracker');
 	});
-	//app.get('api/v1/users/requests',requestController.allRequest);
-	// app.get('api/v1/users/requests/:requestId', requestController.getRequest);
-	// app.post('api/v1/users/requests',requestController.postRequest);
-	// app.put('api/v1/users/requests/:requestId', requestController.updateRequest);
+	
+	app.put('/api/v1/users/requests/:requestId', requestController.updateRequest);
 }
 
 export default routes;
