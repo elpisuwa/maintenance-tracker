87b047352cbce5a09f6c4f55ab8941dd7cd1d25b
uwahope007 <uwahope007@gmail.com>
2018-05-18 03:56:47 +0100

    add routing of api

---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+import requestController from '../../controller/requestController';
+
+const routes =(app)=>{
+	app.get('/', (req,res)=>{
+		res.status(200).send('Welcome to Maintenance Tracker');
+	});
+	app.get('api/v1/users/requests',requestController.allRequest);
+	app.get('api/v1/users/requests/:requestId', requestController.getRequest);
+	app.post('api/v1/users/requests',requestController.postRequest);
+}	app.put('api/v1/users/requests/:requestId', requestController.updateRequest);
+
+
+export default routes;
