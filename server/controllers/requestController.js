257fe4097de1b402d6d585097e82a8be117b2da8
uwahope007 <uwahope007@gmail.com>
2018-05-18 16:00:03 +0100

    update controller

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -7,12 +7,12 @@ class RequestController{
   static postRequest(request,response){
   	const {userId,requestType,requestDescription} = request.body;
   	 const id = data[data.length-1].id+1;
-  	 const searchRequest = {id, userId, requestType,requestDescription};
+  	 const newRequest = {id, userId, requestType,requestDescription};
+  	 
+  	 data.push(newRequest);
 
-  	 data.push(searchRequest);
   	 return response.status(201).json({Request: data, message: 'request as been added successfully'});
- 
-
+ 	
 
 
   }
