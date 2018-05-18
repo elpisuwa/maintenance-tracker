a3718a05fbdb3b364fe699c03ce98a768d10c922
uwahope007 <uwahope007@gmail.com>
2018-05-18 20:19:44 +0100

    update package and test

--- server/tests/test.js
+++ server/tests/test.js
@@ -1,19 +1,23 @@
 import chaiHttp from 'chai-http';
 import chai from 'chai';
 import app from '../app'
-
-const {expect} = chai;
+ 
+// import {expect} from 'chai';
+const should =chai.should();
 chai.use(chaiHttp);
 
 describe('for GET endpoint', ()=>{
   
-  it('should return a 200', (done)->{
+  it('should return a 200', (done)=>{
      chai.request(app)
      .get('/api/v1/users/requests')
      .send({userId:'1',requestType:'repair',requestDescription:'faulty brake pads'} )
      .end((error, response)=>{
-     	expect(response).to.have.status(200);
-     	expect(response.body).to.be.an('object');
+     	// expect(response).to.have.status(200);
+     	// expect(response.body).to.be.an('object');
+     	response.should.have.status(200);
+     	response.should.be.a('object');
+     	done();
      });
   });
 });
