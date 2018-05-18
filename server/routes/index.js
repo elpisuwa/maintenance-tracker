16cd8141ca5f66015a0ed04dc932cd7da262f904
uwahope007 <uwahope007@gmail.com>
2018-05-18 09:45:24 +0100

    comment out routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const routes =(app)=>{
 	app.get('/', (req,res)=>{
 		res.status(200).send('Welcome to Maintenance Tracker');
 	});
-	app.get('api/v1/users/requests',requestController.allRequest);
+	//app.get('api/v1/users/requests',requestController.allRequest);
 	// app.get('api/v1/users/requests/:requestId', requestController.getRequest);
 	// app.post('api/v1/users/requests',requestController.postRequest);
 	// app.put('api/v1/users/requests/:requestId', requestController.updateRequest);
