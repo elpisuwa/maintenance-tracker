34baa6b05b01c34ad0e718c9f35eff46df5a38c4
uwahope007 <uwahope007@gmail.com>
2018-05-19 06:49:41 +0100

    test for update

--- server/tests/test.js
+++ server/tests/test.js
@@ -5,6 +5,17 @@ import app from '../app';
 const {expect} = chai;
 chai.use(chaiHttp);
 
-describe('', ()=>{
+describe('PUT for requests', ()=>{
+	it('should return 200', (done)=>{
+		chai.request(app)
+		.put('/api/v1/users/requests/1')
+		.send({requestType:'repair', requestDescription:'change brake pad'})
+		.end((error,response)=>{
+			expect(response.status).to.equal(200);
+			expect(response.body).to.be.an('object');
+			
+			done();
+		});
+	});
 
 });
