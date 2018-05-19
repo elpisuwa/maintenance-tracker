d77f8cb0509b1744a1166de1e886100682c60d70
uwahope007 <uwahope007@gmail.com>
2018-05-19 05:15:59 +0100

    test for get a request

--- server/tests/test.js
+++ server/tests/test.js
@@ -5,6 +5,15 @@ import app from '../app';
 const {expect} = chai;
 chai.use(chaiHttp);
 
-describe('', ()=>{
+describe('GET endpoint for a request', ()=>{
+	it('should return an 200',(done)=>{
+	chai.request(app)	
+	.get('/api/v1/users/requests/:requestId')
+	.end((error,response)=>{
+		expect(response.status).to.equal(200);
+		expect(response.body).to.be.an('object');
+		done();
+	});
+	});
 
 });
