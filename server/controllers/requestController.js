5d7d0b3ee43fd8464b9b6ad2de02033a32940e7e
uwahope007 <uwahope007@gmail.com>
2018-05-19 07:46:58 +0100

    fixed bug in test

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -8,12 +8,13 @@ class RequestController{
   	const {userId,requestType,requestDescription} = request.body;
   	 const id = data[data.length-1].id+1;
   	 const newRequest = {id, userId, requestType,requestDescription};
-  	 
-  	 data.push(newRequest);
+  	  
+  	 if(userId!=="" && requestType !== "" && requestDescription !== ""){ 
+	  	 data.push(newRequest);
 
-  	 return response.status(201).json({Request: data, message: 'request as been added successfully'});
- 	
-
+	  	 return response.status(201).json({request: data, message: 'Request has been added successfully'});
+	 }
+	     return response.status(400).json({message:'bad request'});
 
   }
 
--- server/tests/test.js
+++ server/tests/test.js
@@ -1,43 +1,44 @@
 import chaiHttp from 'chai-http';
 import chai from 'chai';
 import app from '../app';
-const should = chai.should();
 
-// const {expect} = chai;
+
+const {expect} = chai;
 chai.use(chaiHttp);
 
 describe('POST endpoint for request', ()=>{
 
-	// it('should not add a request with empty type ', (done)=>{
-	// 	chai.request(app)
-	// 	.post('/api/v1/users/requests')
-	// 	.send({userId:1, requestType: '', requestDescription: 'repair wheels'})
-	// 	.end((error,response)=>{
-	// 		response.should.have.status(201);
-	// 		//expect(response.body.error).should.equal('Bad request');
-	// 		done();
-	// 	});
-	// });
+	it('should not add a request with empty type ', (done)=>{
+		chai.request(app)
+		.post('/api/v1/users/requests')
+		.send({userId:1, requestType: '', requestDescription: 'repair wheels'})
+		.end((error,response)=>{
+			expect(response.status).to.equal(400);
+			expect(response.body.message).to.equal('bad request');
+			done();
+		});
+	});
 
-	// it('should not add a request with empty description ', (done)=>{
-	// 	chai.request(app)
-	// 	.post('/api/v1/users/requests')
-	// 	.send({userId:1, requestType: 'repair', requestDescription: ''})
-	// 	.end((error,response)=>{
-	// 		response.should.have.status(201);
-	// 		// expect(response.body.error).to.equal('Bad request');
-	// 		done();
-	// 	});
-	// });
+	it('should not add a request with empty description ', (done)=>{
+		chai.request(app)
+		.post('/api/v1/users/requests')
+		.send({userId:1, requestType: 'repair', requestDescription: ''})
+		.end((error,response)=>{
+			expect(response.status).to.equal(400);
+			expect(response.body.message).to.equal('bad request');
+			done();
+		});
+	});
 
-	it('should return 201 for a successful post ', (done)=>{
+	it('should return 201 for a successful post request ', (done)=>{
 		chai.request(app)
 		.post('/api/v1/users/requests')
 		.send({userId:1, requestType: 'repair', requestDescription: 'fix windscreen'})
 		.end((error,response)=>{
-			response.should.have.status(201);
-			// expect(response.body.message).to.equal('Successfully added request');
-			response.body.should.be.a('object');
+			expect(response.status).to.equal(201);
+			expect(response.body.message).to.equal('Request has been added successfully');
+			expect(response.body).to.be.an('object');
+			expect(response.body.request).to.be.an('array');
 			done();
 		});
 	});
