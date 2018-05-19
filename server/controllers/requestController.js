08d912d42f3c40d7c5d7f0e8e75736d2c9773d89
uwahope007 <uwahope007@gmail.com>
2018-05-19 05:11:37 +0100

    controller for get a request

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -4,19 +4,14 @@ import data from '../data/request.json';
 
 class RequestController{
 
-	static updateRequest(request,response){
-		const {requestType,requestDescription} = request.body;
-
-
+	static viewRequest(request,response){
+	
 		const searchRequest = data.findIndex((userRequest)=>
 			userRequest.id === parseInt(request.params.requestId, 10)
 		);
-
+			// !data[searchRequest] ?  response.status(400).json({message:'doesnt exist'}) :response.status(200).json({request:data[searchRequest]});
 		
-			data[searchRequest].requestType = request.body.requestType;
-			data[searchRequest].description = request.body.requestDescription;
-
-			return response.status(200).json({message:'Request has been successfully updated', request: data})
+		return response.status(200).json({request:data[searchRequest]});
 		
 
 	}
