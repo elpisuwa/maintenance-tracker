f67b2f8d398c69f469c525ab865a203aec455d36
uwahope007 <uwahope007@gmail.com>
2018-05-20 11:33:35 +0100

    update dummy db and test

--- server/tests/test.js
+++ server/tests/test.js
@@ -9,7 +9,7 @@ describe('PUT for requests', ()=>{
 	it('should return 200', (done)=>{
 		chai.request(app)
 		.put('/api/v1/users/requests/1')
-		.send({requestType:'repair', requestDescription:'change brake pad'})
+		.send({model:'volvo', requestType:'repair', requestDescription:'change brake pad'})
 		.end((error,response)=>{
 			expect(response.status).to.equal(200);
 			expect(response.body).to.be.an('object');
