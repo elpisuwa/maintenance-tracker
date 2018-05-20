c9424ba135f100c3641e450421d7b38092704f5b
uwahope007 <uwahope007@gmail.com>
2018-05-20 11:55:55 +0100

    update dummy db and controller

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -6,12 +6,11 @@ class RequestController{
 
 	static viewRequest(request,response){
 	
-		const searchRequest = data.findIndex((userRequest)=>
+		const findRequestId = data.findIndex((userRequest)=>
 			userRequest.id === parseInt(request.params.requestId, 10)
 		);
-			// !data[searchRequest] ?  response.status(400).json({message:'doesnt exist'}) :response.status(200).json({request:data[searchRequest]});
-		
-		return response.status(200).json({request:data[searchRequest]});
+			
+		return response.status(200).json({request:data[findRequestId]});
 		
 
 	}
