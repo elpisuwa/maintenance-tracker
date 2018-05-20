5f651bc19d863fd3208285f561b7ff3ff1a8b5c4
uwahope007 <uwahope007@gmail.com>
2018-05-20 10:07:57 +0100

    add model entity in dummy data

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -5,11 +5,11 @@ import data from '../data/request.json';
 class RequestController{
   
   static postRequest(request,response){
-  	const {userId,requestType,requestDescription} = request.body;
+  	const {userId,model,requestType,requestDescription} = request.body;
   	 const id = data[data.length-1].id+1;
-  	 const newRequest = {id, userId, requestType,requestDescription};
+  	 const newRequest = {id, userId,model,requestType,requestDescription};
   	  
-  	 if(userId!=="" && requestType !== "" && requestDescription !== ""){ 
+  	 if(userId!=="" && model !=="" requestType !== "" && requestDescription !== ""){ 
 	  	 data.push(newRequest);
 
 	  	 return response.status(201).json({request: data, message: 'Request has been added successfully'});
--- server/tests/test.js
+++ server/tests/test.js
@@ -6,12 +6,13 @@ import app from '../app';
 const {expect} = chai;
 chai.use(chaiHttp);
 
+const api = '/api/v1/users/requests';
 describe('POST endpoint for request', ()=>{
 
 	it('should not add a request with empty type ', (done)=>{
 		chai.request(app)
-		.post('/api/v1/users/requests')
-		.send({userId:1, requestType: '', requestDescription: 'repair wheels'})
+		.post(api)
+		.send({userId:1,model:'4runner', requestType: '', requestDescription: 'repair wheels'})
 		.end((error,response)=>{
 			expect(response.status).to.equal(400);
 			expect(response.body.message).to.equal('bad request');
@@ -21,8 +22,19 @@ describe('POST endpoint for request', ()=>{
 
 	it('should not add a request with empty description ', (done)=>{
 		chai.request(app)
-		.post('/api/v1/users/requests')
-		.send({userId:1, requestType: 'repair', requestDescription: ''})
+		.post(api)
+		.send({userId:1,model:'4runner', requestType: 'repair', requestDescription: ''})
+		.end((error,response)=>{
+			expect(response.status).to.equal(400);
+			expect(response.body.message).to.equal('bad request');
+			done();
+		});
+	});
+
+    it('should not add a request with empty description ', (done)=>{
+		chai.request(app)
+		.post(api)
+		.send({userId:1,model:'', requestType: 'repair', requestDescription: 'fix'})
 		.end((error,response)=>{
 			expect(response.status).to.equal(400);
 			expect(response.body.message).to.equal('bad request');
@@ -32,8 +44,8 @@ describe('POST endpoint for request', ()=>{
 
 	it('should return 201 for a successful post request ', (done)=>{
 		chai.request(app)
-		.post('/api/v1/users/requests')
-		.send({userId:1, requestType: 'repair', requestDescription: 'fix windscreen'})
+		.post(api)
+		.send({userId:1, model:'4runner', requestType: 'repair', requestDescription: 'fix windscreen'})
 		.end((error,response)=>{
 			expect(response.status).to.equal(201);
 			expect(response.body.message).to.equal('Request has been added successfully');
