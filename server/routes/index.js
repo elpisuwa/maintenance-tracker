3ab98d4b1f54e010b753971d706ea590900b633e
uwahope007 <uwahope007@gmail.com>
2018-05-24 05:00:04 +0100

    chore-implemented-feedback-[#157715966]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,23 +1,16 @@
 import requestController from '../controllers/requestController';
 
-const routes =(app)=>{
-	app.get('/', (req,res)=>{
-		res.status(200).json({message:'Welcome to Maintenance Tracker'});
-	});
-	
+const routes = (app) => { 
+  app.get('/', (req, res)=>{
+  });
 
-	app.post('/api/v1/users/requests',requestController.postRequest);
+  app.post('/api/v1/users/requests', requestController.postRequest);
 
-	app.get('/api/v1/users/requests/:requestId', requestController.viewRequest);
+  app.get('/api/v1/users/requests/:requestId', requestController.viewRequest);
 
-
-
-	app.get('/api/v1/users/requests',requestController.allRequest);
-  
+  app.get('/api/v1/users/requests', requestController.allRequest);
   app.put('/api/v1/users/requests/:requestId', requestController.updateRequest);
-	
 
 }
-
 
 export default routes;
