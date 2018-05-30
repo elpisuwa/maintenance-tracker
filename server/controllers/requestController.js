913e221526911a5732cb74488a543692b2a53a33
uwahope007 <uwahope007@gmail.com>
2018-05-30 13:24:55 +0100

    feat:user-authorization [Deliver #157833409]

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -4,15 +4,51 @@ import pool from '../models/database';
 
 
 class RequestController {
+
+  static userRequest(request, response) {
+    const userId = request.Id;
+
+    const qry = `SELECT from request WHERE user_id = '${userId}`
+
+    pool.connect((err, client, done) => {
+      if (err) {
+        console.log(`not able to get connection ${err}`);
+        response.status(400).send(err);
+      }
+      client.query(qry)
+        .then(result => response.status(200).json({ request: result.rows }))
+        .catch(next)
+    });
+  }
+
+  // '/api/v1/users/requests/:requestId
+  static viewRequest(request, response, next) {
+    const requestId = request.params.requestId;
+    const qry = `SELECT from requests WHERE id= '${requestId}'`
+    pool.connect((err, client, done) => {
+      if (err) {
+        console.log(`not able to get connection ${err}`);
+        response.status(400).send(err);
+      }
+      client.query(qry)
+        .then(result => response.status(200).json({ requests: result.rows }))
+        .catch(next)
+
+    });
+
+  }
+
+  // '/api/v1/users/requests'
   static postRequest(request, response, next) {
-    const {
-      userId, item, requestType, requestDescription
+    const userId = request.userId;
+    const {id,
+      item, requestType, requestDescription
     } = request.body;
     const status = '';
     // const id =1; fix needs an id to post
     if (userId !== '' && item !== '' && requestType !== '' && requestDescription !== '') {
-      const qry = 'INSERT INTO requests (user_id,item,request_type,request_description,status) VALUES($1, $2, $3, $4, $5) RETURNING * ';
-      const values = [userId, item, requestType, requestDescription, status];
+      const qry = 'INSERT INTO requests (id,user_id,item,request_type,request_description,status) VALUES($1, $2, $3, $4, $5, $6) RETURNING * ';
+      const values = [id, userId, item, requestType, requestDescription, status];
       pool.connect((err, client, done) => {
         if (err) {
           console.log(`not able to get connection ${err}`);
@@ -28,6 +64,32 @@ class RequestController {
     }
   }
 
+  // '/api/v1/users/requests/:requestId'
+  static updateRequest(request, response, next) {
+    const { requestType, requestDescription } = request.body;
+
+    const requestId = request.params.requestId;
+    const findId = `SELECT FROM requests WHERE id= '${requestId}'`;
+
+    const qry = `UPDATE requests SET request_type = '${requestType}', request_description = '${requestDescription}' WHERE requests.id = '${requestId}' RETURNING * `;
+    pool.connect((err, client, done) => {
+      if (err) {
+        console.log(`not able to get connection ${err}`);
+        response.status(400).send(err);
+      }
+      client.query(findId)
+        .then((result1) => {
+          if (result1.rows.length == 0) {
+            return response.status(400).json({ message: 'Request does not exist' });
+          }
+          client.query(qry)
+            .then(result => response.status(200).json({ message: 'Request has been successfully updated', request: result.rows[0] }))
+            .catch(next);
+
+        });
+    });
+  }
+  // /api/v1/requests
   static allRequest(request, response, next) {
 
     const qry = 'SELECT * FROM requests';
@@ -43,13 +105,9 @@ class RequestController {
     });
   }
 
-  static userRequest(request, response) {
-    const newData = data.filter(userRequest =>
-      userRequest.userId == parseInt(request.params.userId, 10));
-    return response.status(200).json({ newData });
 
-  }
 
+  // '/api/v1/requests/:requestId/approve'
   static approveRequest(request, response, next) {
     // rewrite this logic, check if it is null first then and exist before Updating
     // const findQuery = `SELECT from requests WHERE requests.id ='${request.param.requestId}'`;
@@ -79,7 +137,7 @@ class RequestController {
   }
 
 
-
+  // '/api/v1/requests/:requestId/disapprove'
   static disapproveRequest(request, response, next) {
     const { status } = request.body;
     if (status !== '' && status === 'disapprove') {
@@ -105,7 +163,8 @@ class RequestController {
     } else { return response.status(400).json({ message: 'Invalid entry, Ensure you enter: disapprove' }) }
   }
 
-  static resolveRequest(request, response) {
+  // '/api/v1/requests/:requestId/resolve
+  static resolveRequest(request, response, next) {
     const { status } = request.body;
     if (status !== '' && status === 'resolve') {
       const addStatus = 'resolve';
@@ -131,36 +190,10 @@ class RequestController {
   }
 
 
-  static updateRequest(request, response, next) {
-    const { requestType, requestDescription } = request.body;
-
-    const requestId = request.params.requestId;
-    const findId = `SELECT FROM requests WHERE id= '${requestId}'`;
-
-    const qry = `UPDATE requests SET request_type = '${requestType}', request_description = '${requestDescription}' WHERE requests.id = '${requestId}' RETURNING * `;
-    pool.connect((err, client, done) => {
-      if (err) {
-        console.log(`not able to get connection ${err}`);
-        response.status(400).send(err);
-      }
-      client.query(findId)
-        .then((result1) => {
-          if (result1.rows.length == 0) {
-            return response.status(400).json({ message: 'Request does not exist' });
-          }
-          client.query(qry)
-            .then(result => response.status(200).json({ message: 'Request has been successfully updated', request: result.rows[0] }))
-            .catch(next);
-
-        });
-    });
-  }
 
 
-  static viewRequest(request, response) {
-    const findRequestId = data.findIndex(userRequest =>
-      userRequest.id === parseInt(request.params.requestId, 10));
-    return response.status(200).json({ request: data[findRequestId] });
-  }
+
+
+
 }
 export default RequestController;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,23 +1,25 @@
 import requestController from '../controllers/requestController';
 import userController from '../controllers/userController';
-
+import VerifyToken from '../controllers/auth/VerifyToken';
 const routes = (app) => {
   app.get('/', (req, res) => {
     res.send('welcome to Maintenance Tracke!');
   });
-  app.post('/api/v1/users/requests', requestController.postRequest);
+  app.post('/api/v1/users/requests', VerifyToken, requestController.postRequest);
+  app.get('/api/v1/requests', VerifyToken, requestController.allRequest);
+  app.get('/api/v1/users/requests/:requestId', VerifyToken, requestController.viewRequest);
+  app.get('/api/v1/users/requests', VerifyToken, requestController.userRequest);
   app.get('/api/v1/requests', requestController.allRequest);
-  app.get('/api/v1/users/requests/:requestId', requestController.viewRequest);
+  app.put('/api/v1/users/requests/:requestId', VerifyToken, requestController.updateRequest);
 
-  app.get('/api/v1/users/requests', requestController.allRequest);
-  app.put('/api/v1/users/requests/:requestId', requestController.updateRequest);
-
-  app.get('/api/v1/users/:userId/requests', requestController.userRequest);
-  app.put('/api/v1/requests/:requestId/approve', requestController.approveRequest);
-  app.put('/api/v1/requests/:requestId/disapprove', requestController.disapproveRequest);
-  app.put('/api/v1/requests/:requestId/resolve', requestController.resolveRequest);
+  //app.get('/api/v1/users/:userId/requests', requestController.userRequest);
+  app.put('/api/v1/requests/:requestId/approve',VerifyToken, requestController.approveRequest);
+  app.put('/api/v1/requests/:requestId/disapprove',VerifyToken, requestController.disapproveRequest);
+  app.put('/api/v1/requests/:requestId/resolve', VerifyToken, requestController.resolveRequest);
   app.post('/auth/login', userController.loginRequest);
   app.post('/auth/signup', userController.signupRequest);
+  app.get('/logout', userController.logout);
+  app.get('/user', VerifyToken, userController.user)
 }
 
 export default routes;
