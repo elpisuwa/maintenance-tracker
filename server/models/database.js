adc1353da306aaecfbc156d347309be7781c3e34
uwahope007 <uwahope007@gmail.com>
2018-05-30 19:08:44 +0100

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,6 +1,6 @@
 import pg from 'pg';
 //import config from '../../config';
-const connectionString = 'process.env.DATABASE_URL || 'postgresql://postgres:uwahope007@localhost:5432/mtracker';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:uwahope007@localhost:5432/mtracker';
 const pool = new pg.Pool(connectionString);
 
 
