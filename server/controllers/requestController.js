754be68d3a6888ac856b0efeb1b664d577c08750
uwahope007 <uwahope007@gmail.com>
2018-05-30 04:37:04 +0100

    feat: approve-requests-endpoint[Delivered #15783483]

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -9,7 +9,7 @@ class RequestController {
       userId, item, requestType, requestDescription
     } = request.body;
     const status = '';
-    //const id =1; fix needs an id to post
+    // const id =1; fix needs an id to post
     if (userId !== '' && item !== '' && requestType !== '' && requestDescription !== '') {
       const qry = 'INSERT INTO requests (user_id,item,request_type,request_description,status) VALUES($1, $2, $3, $4, $5) RETURNING * ';
       const values = [userId, item, requestType, requestDescription, status];
@@ -23,26 +23,25 @@ class RequestController {
           .catch(next)
 
       });
-    }
-    else{
-    return response.status(400).json({ message: 'An Empty field found, Please fill up all fields', error: 'Bad Request' });
+    } else {
+      return response.status(400).json({ message: 'An Empty field found, Please fill up all fields', error: 'Bad Request' });
     }
   }
 
   static allRequest(request, response, next) {
 
-  const qry = `SELECT * FROM requests`;
-  pool.connect((err, client, done) => {
-        if (err) {
-          console.log(`not able to get connection ${err}`);
-          response.status(400).send(err);
-        }
-        client.query(qry)
-          .then(result => response.status(200).json({ requests: result }))
-          .catch(next)
+    const qry = 'SELECT * FROM requests';
+    pool.connect((err, client, done) => {
+      if (err) {
+        console.log(`not able to get connection ${err}`);
+        response.status(400).send(err);
+      }
+      client.query(qry)
+        .then(result => response.status(200).json({ requests: result.rows }))
+        .catch(next)
 
-  });
-}
+    });
+  }
 
   static userRequest(request, response) {
     const newData = data.filter(userRequest =>
@@ -51,17 +50,35 @@ class RequestController {
 
   }
 
-  static approveRequest(request, response) {
+  static approveRequest(request, response, next) {
+    //rewrite this logic, check if it is null first then and exist before Updating 
+   // const findQuery = `SELECT from requests WHERE requests.id ='${request.param.requestId}'`;
     const { status } = request.body;
+    if (status !== '' && status === 'approve') {
+      const addStatus = 'pending';
+      const requestId = request.params.requestId;
+      const qry = `UPDATE requests SET status = '${addStatus}' WHERE requests.id = '${requestId}' RETURNING * `;
+      pool.connect((err, client, done) => {
+        if (err) {
+          console.log(`not able to get connection ${err}`);
+          response.status(400).send(err);
+        }
+        client.query(qry)
+          .then((result) => {
+            if (result.rows.length==0) {
+              response.status(400).json({ message: 'This request does not exist' });
+            } else {
+              response.status(201).json({ message: 'Request has been Approved', request: result.rows });
+            }
+          })
+          .catch(next)
 
-    const findId = data.findIndex(userRequest =>
-      userRequest.id == parseInt(request.params.requestId, 10));
-    if (request.body.status === 'pending') {
-      data[findId].status = request.body.status;
-      return response.status(201).json({ message: 'Request has been Approved', request: data[findId] });
-    }
-    return response.status(400).json({ message: 'Invalid entry, Ensure you entered: pending' })
+      });
+    } else { return response.status(400).json({ message: 'Invalid entry, Ensure you enter: approve' }) }
+
   }
+
+
 
   static disapproveRequest(request, response) {
     const { status } = request.body;
