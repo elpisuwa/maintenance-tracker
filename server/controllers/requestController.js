c1f75f42d73762e5b0c5990b2959b76ace8aafc1
uwahope007 <uwahope007@gmail.com>
2018-05-30 09:09:43 +0100

    feat: modify-request-endpoint-#157882542

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -51,8 +51,8 @@ class RequestController {
   }
 
   static approveRequest(request, response, next) {
-    //rewrite this logic, check if it is null first then and exist before Updating 
-   // const findQuery = `SELECT from requests WHERE requests.id ='${request.param.requestId}'`;
+    // rewrite this logic, check if it is null first then and exist before Updating
+    // const findQuery = `SELECT from requests WHERE requests.id ='${request.param.requestId}'`;
     const { status } = request.body;
     if (status !== '' && status === 'approve') {
       const addStatus = 'pending';
@@ -65,7 +65,7 @@ class RequestController {
         }
         client.query(qry)
           .then((result) => {
-            if (result.rows.length==0) {
+            if (result.rows.length == 0) {
               response.status(400).json({ message: 'This request does not exist' });
             } else {
               response.status(201).json({ message: 'Request has been Approved', request: result.rows });
@@ -93,7 +93,7 @@ class RequestController {
         }
         client.query(qry)
           .then((result) => {
-            if (result.rows.length==0) {
+            if (result.rows.length == 0) {
               response.status(400).json({ message: 'This request does not exist' });
             } else {
               response.status(201).json({ message: 'Request has been Disapproved', request: result.rows });
@@ -118,7 +118,7 @@ class RequestController {
         }
         client.query(qry)
           .then((result) => {
-            if (result.rows.length==0) {
+            if (result.rows.length == 0) {
               response.status(400).json({ message: 'This request does not exist' });
             } else {
               response.status(201).json({ message: 'Request has been Resolved', request: result.rows });
@@ -131,17 +131,32 @@ class RequestController {
   }
 
 
-  static updateRequest(request, response) {
+  static updateRequest(request, response, next) {
     const { requestType, requestDescription } = request.body;
-    const findRequestId = data.findIndex(userRequest =>
-      userRequest.id === parseInt(request.params.requestId, 10));
-    data[findRequestId].requestType = request.body.requestType;
-    data[findRequestId].requestDescription = request.body.requestDescription;
-    if (data[findRequestId].status === 'pending') {
-      return response.status(200).json({ message: 'Request has been successfully updated', request: data[findRequestId] })
-    }
-    return response.status(400).json({ message: 'Request can not be updated yet, until Approved' })
+
+    const requestId = request.params.requestId;
+    const findId = `SELECT FROM requests WHERE id= '${requestId}'`;
+
+    const qry = `UPDATE requests SET request_type = '${requestType}', request_description = '${requestDescription}' WHERE requests.id = '${requestId}' RETURNING * `;
+    pool.connect((err, client, done) => {
+      if (err) {
+        console.log(`not able to get connection ${err}`);
+        response.status(400).send(err);
+      }
+      client.query(findId)
+        .then((result1) => {
+          if (result1.rows.length == 0) {
+            return response.status(400).json({ message: 'Request does not exist' });
+          }
+          client.query(qry)
+            .then(result => response.status(200).json({ message: 'Request has been successfully updated', request: result.rows[0] }))
+            .catch(next);
+
+        });
+    });
   }
+
+
   static viewRequest(request, response) {
     const findRequestId = data.findIndex(userRequest =>
       userRequest.id === parseInt(request.params.requestId, 10));
