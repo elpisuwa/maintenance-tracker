df7f78d62615a9eb01662cd2e0a5d2237519770f
uwahope007 <uwahope007@gmail.com>
2018-06-01 09:00:25 +0100

    Update requestController.js

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -24,7 +24,7 @@ class RequestController {
   // '/api/v1/users/requests/:requestId.
   static viewRequest(request, response) {
     const requestId = request.params.requestId;
-    if (Number.isInteger(requestId) === true) { 
+    if (requestId === parseInt(requestId, 10)) { 
       const qry = `SELECT * FROM requests WHERE id= '${requestId}'`
       pool.connect((err, client, done) => {
         if (err) {
